ecd654767613712b828f06384aa2bb5ea4388f98
root <root@windows-phone.vng.com.vn>
2015-08-05 17:04:30 +0700

    - fix: usb --kill option before start atcd - enable debug
    Signed-off-by: duydb2 <duydb2@gmail.com>

--- atcui/atcui/settings.py
+++ atcui/atcui/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'luj_jam&2nca+r$5sh_l7rm5tu3zl$u!d&h!f*0hji2x9av!zm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True 
 
 ALLOWED_HOSTS = ['*']
 
